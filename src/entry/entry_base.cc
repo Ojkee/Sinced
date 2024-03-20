8e430c1fd62b46601ee2ef6ea470eff3c7c23c7c
Ojkee <17oju17@gmail.com>
2024-03-20 13:39:24 +0100

    Did you know that optional deadline is more optional than unique?

--- src/entry/entry_base.cc
+++ src/entry/entry_base.cc
@@ -25,7 +25,7 @@ void EntryTask::tokenize(const std::string &line) {
   status = static_cast<Status>(std::stoi(tokens[2]));
   if (tokens[3] != "-1") {
     const uint16_t int_date = (uint16_t)std::stoul(tokens[3]);
-    deadline = std::make_unique<BaseDate>(BaseDate::days_to_date(int_date));
+    deadline = std::make_optional<BaseDate>(BaseDate::days_to_date(int_date));
   }
   r_days = static_cast<uint16_t>(std::stoul(tokens[4]));
   r_months = static_cast<uint16_t>(std::stoul(tokens[5]));
@@ -33,8 +33,7 @@ void EntryTask::tokenize(const std::string &line) {
 }
 
 EntryTask::operator std::string() const {
-  const int date_str =
-      (deadline == nullptr) ? -1 : BaseDate::date_to_days(*deadline);
+  const int date_str = (deadline) ? -1 : BaseDate::date_to_days(*deadline);
   return std::format("{} \"{}\" {} {} {} {} {}", id, content,
                      static_cast<uint16_t>(status), date_str, r_days, r_months,
                      r_years);
@@ -75,9 +74,9 @@ std::string EntryTask::info() const {
                      deadline_str + repetetive_str);
 }
 
-[[nodiscard]] BaseDate EntryTask::next_repetetive_deadline() const {
+BaseDate EntryTask::next_repetetive_deadline() const {
   BaseDate today_date = BaseDate::today();
-  BaseDate result = BaseDate::shallow_copy_from_unique(deadline);
+  BaseDate result = BaseDate::shallow_copy(std::move(*deadline));
   for (std::size_t i = 0; i < 200000; ++i) {
     if (result >= today_date) {
       return result;
@@ -116,12 +115,11 @@ EntryRelation::operator std::string() const {
 
 std::string EntryRelation::info() const { return std::string(*this); }
 
-[[nodiscard]] bool
-EntryTask::is_in_timeframe(const std::unique_ptr<BaseDate> &date_) const {
+bool EntryTask::is_in_timeframe(const BaseDate &date_) const {
   BaseDate today = BaseDate::today();
   if (is_repetetive()) {
     BaseDate nextDeadline = next_repetetive_deadline();
-    return nextDeadline <= *date_;
+    return nextDeadline <= date_;
   }
-  return (deadline) ? today <= *deadline && *deadline <= *date_ : false;
+  return (deadline) ? today <= *deadline && *deadline <= date_ : false;
 }
